fa6b681678646d3962462b0a8170ff7ac58abb76
dieglos18 <diegoarmando1803@gmail.com>
2025-05-25 19:31:26 -0400

    feat: create styles const

---
+++ gericht/src/constants/styles.ts
@@ -0,0 +1,4 @@
+export const p__opensans =
+  "font-alt text-white font-medium tracking-[0.04em] capitalize leading-[28px] text-[16px]";
+export const p__cormorant =
+  "font-base text-white tracking-[0.06em] capitalize leading-[29.9px] text-[23px] [font-feature-settings:'tnum_on','lnum_on']";
