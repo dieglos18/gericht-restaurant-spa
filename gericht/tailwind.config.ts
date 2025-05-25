46854ddcf114ce058a405cc76dd1f1d9aa6791c4
dieglos18 <diegoarmando1803@gmail.com>
2025-05-25 11:15:08 -0400

    add tailwind configurations

--- gericht/tailwind.config.ts
+++ gericht/tailwind.config.ts
@@ -1,10 +1,37 @@
 export default {
-  content: [
+   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        base: ['"Cormorant Upright"', "serif"],
+        alt: ['"Open Sans"', "sans-serif"],
+      },
+      colors: {
+        golden: "#DCCA87",
+        dark: "#0C0C0C",
+        gray: "#545454",
+        crimson: "#F5EFDB",
+        grey: "#AAAAAA",
+        light: "#FFFFFF",
+      },
+      keyframes: {
+        "slide-bottom": {
+          "0%": {
+            transform: "translateY(-25%)",
+          },
+          "100%": {
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "slide-bottom":
+          "slide-bottom 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
+      },
+    },
   },
   plugins: [],
 };
